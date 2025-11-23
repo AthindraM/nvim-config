b6da5001054ec24b2c59df9352e438c3dd3261c0
Athindra Mitra <athindra.nm4@gmail.com>
2025-11-22 22:36:52 -0500

    added .gitignore and colorscheme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,36 @@
 return {
+	-- Onedark colorscheme
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000,  -- load before other UI plugins
+		config = function()
+			require("onedark").setup {
+				style = "darker",  -- options: dark, darker, cool, deep, warm, warmer, light
+			}
+			require("onedark").load()
+		end,
+  },
 
+	-- nvim-tree
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("nvim-tree").setup({
+				sort = {
+					sorter = "case_sensitive",
+				},
+				view = {
+					width = 30,
+				},
+				renderer = {
+					group_empty = true,
+				},
+				filters = {
+					dotfiles = true,
+				},
+			})
+		end,
+	},
 }
+
