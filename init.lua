8170a38f88b8128fbdbaa4f374f9ff0ba87b1820
Athindra Mitra <athindra.nm4@gmail.com>
2025-11-22 21:55:21 -0500

    added tab opts

--- init.lua
+++ init.lua
@@ -3,5 +3,11 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- tabs
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.tabstop = 2
+
+-- misc
+vim.opt.termguicolors = -- true color support
+
 -- requirements
-require("config.lazy")
