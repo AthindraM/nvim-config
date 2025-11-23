d0a75849cadbf796d60f70c9da6eaa9f1efd66db
Athindra Mitra <athindra.nm4@gmail.com>
2025-11-23 11:31:00 -0500

    added autopairs plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -32,5 +32,12 @@ return {
 			})
 		end,
 	},
+	{
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+	},
 }
 
