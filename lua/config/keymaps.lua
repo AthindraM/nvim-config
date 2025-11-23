88d552b2442d82b25c54d565cdf767ed2083123b
Athindra Mitra <athindra.nm4@gmail.com>
2025-11-23 11:54:55 -0500

    added telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,7 @@ vim.keymap.set("i", "jk", "<esc>")
 
 -- PLUGINS --
 -- nvim tree
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file explorer" })
+
+-- telescope
+vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files" })
