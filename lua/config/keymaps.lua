6302a722ff2a57a31128f691bf5ce0f99a70de58
Athindra Mitra <athindra.nm4@gmail.com>
2025-11-24 12:16:32 -0500

    nvm.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,20 +2,16 @@ vim.keymap.set("i", "jk", "<esc>")
 
 -- c++
 vim.keymap.set("n", "<leader>cb", "<cmd>w<cr><cmd>!cmake -Bbuild && cmake --build build<cr>",
-  { desc = "Build project" } )
+  { desc = "Build project" })
 
 vim.keymap.set("n", "<leader>cr", ":!./build/OpenGLProject<CR>",
-  { desc = "Run C++ project" } )
-
--- python
-vim.keymap.set("n","<leader>pr", "<cmd>w<cr><cmd>!python3 main.py"),
-	{ desc = "Run Python project" }
+  { desc = "Run C++ project" })
 
 -- PLUGINS --
 -- nvim tree
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>",
-	{ desc = "Toggle file explorer" })
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", 
+  { desc = "Toggle file explorer" })
 
 -- telescope
 vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<cr>",
-	{ desc = "Fuzzy find files" })
+  { desc = "Fuzzy find files" })
