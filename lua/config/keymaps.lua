654232f822dc14730f46825fb5996acfa4c07baf
Athindra Mitra <athindra.nm4@gmail.com>
2025-11-23 19:12:09 -0500

    added keymaps to build and run c++ prorgams

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,17 @@
 vim.keymap.set("i", "jk", "<esc>")
 
+-- c++
+vim.keymap.set("n", "<leader>b", ":!cmake -Bbuild && cmake --build build<CR>",
+  { desc = "Build project" })
+
+vim.keymap.set("n", "<leader>r", ":!./build/OpenGLProject<CR>",
+  { desc = "Run project" })
+
 -- PLUGINS --
 -- nvim tree
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>",
+	{ desc = "Toggle file explorer" })
 
 -- telescope
-vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files" })
+vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<cr>",
+	{ desc = "Fuzzy find files" })
