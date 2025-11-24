623fc72886f2ea1bbd93557405ebbf15f48d9b70
Athindra Mitra <athindra.nm4@gmail.com>
2025-11-24 18:53:41 -0500

    made it so that the default statusline doesnt show

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -6,6 +6,26 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.tabstop = 2
+vim.opt.autoindent = true
 
--- misc
+-- line wrapping
+vim.opt.wrap = false
+
+-- search settings
+vim.opt.ignorecase = true -- ignore case when searching
+vim.opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- cursor line
+vim.opt.cursorline = true -- highlight the current cursor line
+
+-- appearance
 vim.opt.termguicolors = true -- true color support
+vim.opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+vim.opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+vim.opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- statusline
+vim.o.laststatus = 3
+vim.o.showmode = false
