f64f3b5c10250dcfbf868e9bd59fffca29a96114
Athindra Mitra <athindra.nm4@gmail.com>
2025-11-24 12:14:23 -0500

    added keymap to run python projects

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,15 @@
 vim.keymap.set("i", "jk", "<esc>")
 
 -- c++
-vim.keymap.set("n", "<leader>cb", "<cmd>w<cr>:!cmake -Bbuild && cmake --build build<cr>",
-  { desc = "Build project" })
+vim.keymap.set("n", "<leader>cb", "<cmd>w<cr><cmd>!cmake -Bbuild && cmake --build build<cr>",
+  { desc = "Build project" } )
 
 vim.keymap.set("n", "<leader>cr", ":!./build/OpenGLProject<CR>",
-  { desc = "Run project" })
+  { desc = "Run C++ project" } )
+
+-- python
+vim.keymap.set("n","<leader>pr", "<cmd>w<cr><cmd>!python3 main.py"),
+	{ desc = "Run Python project" }
 
 -- PLUGINS --
 -- nvim tree
