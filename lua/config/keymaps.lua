e4d05c73643be440e5bc22174679fdc76d1b6b06
Athindra Mitra <athindra.nm4@gmail.com>
2025-12-17 01:15:28 -0500

    added a way to cycle and select different themes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,19 @@ vim.keymap.set("n", "<leader>cb", "<cmd>w<cr><cmd>!cmake -Bbuild && cmake --buil
   { desc = "Build C++ project" })
 
 -- PLUGINS --
+-- Colorscheme keymaps
+vim.keymap.set("n", "<leader>tn", function()
+  _G.cycle_colorscheme("next")
+end, { desc = "Next theme" })
+
+vim.keymap.set("n", "<leader>tp", function()
+  _G.cycle_colorscheme("prev")
+end, { desc = "Previous theme" })
+
+vim.keymap.set("n", "<leader>ts", function()
+  _G.select_colorscheme()
+end, { desc = "Select theme" })
+
 -- nvim tree
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", 
   { desc = "Toggle file explorer" })
@@ -12,3 +25,4 @@ vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>",
 -- telescope
 vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<cr>",
   { desc = "Fuzzy find files" })
+
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
--- onedark colorscheme
 return {
+	-- onedark colorscheme
 	"navarasu/onedark.nvim",
 	priority = 1000,  -- load before other UI plugins
 	config = function()
@@ -7,5 +7,67 @@ return {
 			style = "darker",  -- options: dark, darker, cool, deep, warm, warmer, light
 		}
 		require("onedark").load()
+
+		-- Define your colorscheme list (accessible globally)
+    _G.colorschemes = {
+      "onedark",
+      "habamax",
+      "slate",
+      "desert",
+      "evening",
+      "industry",
+      "koehler",
+      "morning",
+      "murphy",
+      "pablo",
+      "peachpuff",
+      "ron",
+      "shine",
+      "torte",
+      "zellner",
+      "blue",
+      "darkblue",
+      "delek",
+      "elflord",
+    }
+    
+    _G.current_colorscheme_index = 1
+    
+    -- Function to cycle through colorschemes (accessible globally)
+    _G.cycle_colorscheme = function(direction)
+      if direction == "next" then
+        _G.current_colorscheme_index = _G.current_colorscheme_index + 1
+        if _G.current_colorscheme_index > #_G.colorschemes then
+          _G.current_colorscheme_index = 1
+        end
+      else
+        _G.current_colorscheme_index = _G.current_colorscheme_index - 1
+        if _G.current_colorscheme_index < 1 then
+          _G.current_colorscheme_index = #_G.colorschemes
+        end
+      end
+      
+      local scheme = _G.colorschemes[_G.current_colorscheme_index]
+      vim.cmd.colorscheme(scheme)
+      print("Colorscheme: " .. scheme)
+    end
+    
+    -- Function to select colorscheme from menu
+    _G.select_colorscheme = function()
+      vim.ui.select(_G.colorschemes, {
+        prompt = "Select colorscheme:",
+      }, function(choice)
+        if choice then
+          vim.cmd.colorscheme(choice)
+          -- Update current_index to match selection
+          for i, scheme in ipairs(_G.colorschemes) do
+            if scheme == choice then
+              _G.current_colorscheme_index = i
+              break
+            end
+          end
+        end
+      end)
+    end
 	end,
 }
